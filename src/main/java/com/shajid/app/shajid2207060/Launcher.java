df0eea352d5f67c3565559b18e3f56ac99edc41f
MAJHARUL SHAJID <shajid2207060@stud.kuet.ac.bd>
2025-11-10 01:13:17 +0600

    first commit

---
+++ src/main/java/com/shajid/app/shajid2207060/Launcher.java
@@ -0,0 +1,9 @@
+package com.shajid.app.shajid2207060;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
