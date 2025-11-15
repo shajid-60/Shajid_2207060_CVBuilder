cdade5c0779bf6050a9f723550c7a829d8e1a659
MAJHARUL SHAJID <shajid2207060@stud.kuet.ac.bd>
2025-11-15 18:57:29 +0600

    Form fxml and controller is created

--- src/main/java/com/shajid/app/shajid2207060/controller/FormController.java
+++ src/main/java/com/shajid/app/shajid2207060/controller/FormController.java
@@ -1,4 +1,48 @@
 package com.shajid.app.shajid2207060.controller;
+import com.shajid.app.shajid2207060.model.CVData;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class FormController {
+
+    @FXML private TextField fullNameField;
+    @FXML private TextField emailField;
+    @FXML private TextField phoneField;
+    @FXML private TextField addressField;
+
+    @FXML private TextArea educationArea;
+    @FXML private TextArea skillsArea;
+    @FXML private TextArea experienceArea;
+    @FXML private TextArea projectsArea;
+
+    @FXML
+    private void handleGenerateCV() {
+
+        CVData.fullName = fullNameField.getText();
+        CVData.email = emailField.getText();
+        CVData.phone = phoneField.getText();
+        CVData.address = addressField.getText();
+        CVData.education = educationArea.getText();
+        CVData.skills = skillsArea.getText();
+        CVData.experience = experienceArea.getText();
+        CVData.projects = projectsArea.getText();
+
+        try {
+            FXMLLoader loader = new FXMLLoader(
+                    getClass().getResource("/com/shajid/app/shajid2207060/Preview.fxml")
+            );
+
+            Stage stage = new Stage();
+            stage.setScene(new Scene(loader.load(), 900, 650));
+            stage.setTitle("CV Preview");
+            stage.show();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/com/shajid/app/shajid2207060/controller/HomeController.java
+++ src/main/java/com/shajid/app/shajid2207060/controller/HomeController.java
@@ -11,12 +11,14 @@ public class HomeController {
     public void handleCreateCV(ActionEvent event) {
         try {
             FXMLLoader loader = new FXMLLoader(
-                    getClass().getResource("/com/shajid/app/shajid2207060/Form.fxml")
+                    HomeController.class.getResource("/com/shajid/app/shajid2207060/Form.fxml")
             );
 
             Scene scene = new Scene(loader.load(), 900, 650);
+
             Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
             stage.setScene(scene);
+            stage.show();
 
         } catch (Exception e) {
             e.printStackTrace();
